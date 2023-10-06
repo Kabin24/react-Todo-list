a634d89a3a1988c9f91cc5cfa5a09d64027da683
kabin shrestha <108089715+Kabin24@users.noreply.github.com>
2023-10-06 22:44:40 +0545

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import React from "react";
+import React from 'react';
+import './App.css';
+import TodoList from './components/TodoList';
 
+function App() {
+  return (
+    <div className='todo-app'>
+      <TodoList />
+    </div>
+  );
+}
 
-
- function App(){
-    return (
-        <div className="todo-app">
-            <h1>To appp</h1>
-        </div>
-
-    )
- }
-
- export default App;
+export default App;
